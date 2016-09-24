ceb84ebba5386dcad575986add1132bd77ab841d
Broke116 <ekiny018@gmail.com>
2016-09-24 14:23:42 +0300

    service class library and encryptionservice logic have been added

---
+++ CarRental/CarRental.Service/EncryptionService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using CarRental.Service.Interfaces;
+
+namespace CarRental.Service
+{
+    public class EncryptionService : IEncryptionService
+    {
+        public string CreateSalt()
+        {
+            var data = new byte[0x10];
+            using (var cryptoServiceProvider = new RNGCryptoServiceProvider())
+            {
+                cryptoServiceProvider.GetBytes(data);
+                return Convert.ToBase64String(data);
+            }
+        }
+
+        public string EncryptPassword(string password, string salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var saltedPassword = $"{salt}{password}";
+                byte[] saltedPasswordBytes = Encoding.UTF8.GetBytes(saltedPassword);
+                return Convert.ToBase64String(sha256.ComputeHash(saltedPasswordBytes));
+            }
+        }
+    }
+}
---
+++ CarRental/CarRental.Service/Interfaces/IEncryptionService.cs
@@ -0,0 +1,8 @@
+﻿namespace CarRental.Service.Interfaces
+{
+    public interface IEncryptionService
+    {
+        string CreateSalt();
+        string EncryptPassword(string password, string salt);
+    }
+}
