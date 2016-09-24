38c1fbc79c265112d60178e575c29f0931b01857
Broke116 <ekiny018@gmail.com>
2016-09-24 14:58:51 +0300

    mvc web application added to the solution.then custom authorize attributes has beenadded

---
+++ CarRental/CarRental.Web/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using CarRental.Web.Structure.Authorize;
+
+namespace CarRental.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected new virtual CustomPrincipal User
+        {
+            get { return HttpContext.User as CustomPrincipal; }
+        }
+    }
+}
