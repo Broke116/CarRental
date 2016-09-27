7ade81dd1a278594ab3f199aa40ebecc6311e123
Broke116 <ekiny018@gmail.com>
2016-09-28 00:58:41 +0300

    register car page operations

--- CarRental/CarRental.Web/Controllers/CarController.cs
+++ CarRental/CarRental.Web/Controllers/CarController.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CarRental.Data.App_Data;
+using CarRental.Data.Infastructure;
+using CarRental.Data.Repositories;
 
 namespace CarRental.Web.Controllers
 {
-    [Authorize]
-    public class CarController : Controller
+    //[Authorize]
+    public class CarController : BaseController
     {
+        public CarController(IBaseRepository<Error> _errorsRepository, IUnitOfWork _unitOfWork)
+            : base(_errorsRepository, _unitOfWork)
+        {
+        }
+
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddCar()
         {
             return View();
         }
