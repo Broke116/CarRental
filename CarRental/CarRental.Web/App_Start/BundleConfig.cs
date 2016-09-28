1a214d5ce0310c3770a3f98351b54e79ace1bae2
Broke116 <ekiny018@gmail.com>
2016-09-29 01:14:00 +0300

    scripts and content files optimized

--- CarRental/CarRental.Web/App_Start/BundleConfig.cs
+++ CarRental/CarRental.Web/App_Start/BundleConfig.cs
@@ -7,23 +7,23 @@ namespace CarRental.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/bower_components/jquery/dist/jquery.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-            
+                        "~/bower_components/jquery.validate/dist/jquery.validate.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/bower_components/modernizr/modernizr.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/bower_components/bootstrap/dist/js/bootstrap.js",
+                      "~/bower_components/respond/src/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css/bootstrap").Include(
-                "~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
+                "~/bower_components/bootstrap/dist/css/bootstrap.css", new CssRewriteUrlTransform()));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/site.css").Include("~/Content/font-awesome.min.css", new CssRewriteUrlTransform()));
+                      "~/Content/site.css").Include("~/bower_components/font-awesome/css/font-awesome.min.css", new CssRewriteUrlTransform()));
         }
     }
 }
